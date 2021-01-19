2d19ddec58d8720837194f8ab15e7b1d8269ce5d
Michael <Michael@DESKTOP-82963K8>
2021-01-19 22:42:04 +1100

    Refactored code, added commit option, more robust configuration options

--- import.py
+++ import.py
@@ -3,23 +3,20 @@
 # NOTE: Requires cryptography 3+ module (for PFX functionality)
 #Author - Michael Bell
 import xml.etree.ElementTree as ET
-import requests, json, base64, random, string
+import requests, json, base64, random, string, argparse
 from cryptography import x509
 import cryptography
 
 from cryptography.hazmat.primitives.serialization import load_pem_private_key, pkcs12
 
-
-
 import credentials
 
-ACME_JSON_LOCATION = credentials.traefik_acme_location if hasattr(credentials, "traefik_acme_location") else "acme.json"
-
-def get_acme_json():
-    f = open(ACME_JSON_LOCATION, 'r')
+def get_acme_json(string: acme_json_file):
+    f = open(acme_json_file, 'r')
     contents = f.read()
     f.close()
     return contents
+
 
 def get_certificates(acme_json_contents):
     j = json.loads(acme_json_contents)
@@ -37,7 +34,8 @@ def get_certificates(acme_json_contents):
         certs_out.append(dictobj)
     return certs_out
 
-#Iterates through the certificate byte data, checks the common name, and returns if it matches common_name
+#Iterates through the certificate byte data, checks the common name
+#Then returns if it matches common_name
 def select_certificate(le_dict_list, common_name):
     for le_dict in le_dict_list:
         pem_bytes = le_dict['certificate']
@@ -53,8 +51,35 @@ def get_random_alphanumeric_string(length: int):
     result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
     return result_str
 
+def generate_api_key(hostname: string, username: string, password: string, disable_ssl_validation: bool):
+    xmlapi_key = requests.get("https://" + hostname + "/api/?type=keygen&user=" + 
+                              username + "&password=" + password,verify=not disable_ssl_validation)
+    xmlapi_key = ET.fromstring(xmlapi_key.text).find(".//key").text
+    return xmlapi_key
+
+def check_panos_errors(response):
+    if isinstance(root_node, str):
+        root_node = ET.fromstring(root_node)
+    if root_node.attrib['status'] == 'success':
+        if 'code' not in root_node.attrib:
+            return True
+        if root_node.attrib['code'] in ('19', '20'):
+            return True
+        elif root_node.attrib['code'] in ('7'):
+            print("[WARN] Response returned success, but object not present")
+            return True
+        else:
+            print("[WARN] Response returned success, but non standard response code: " + root_node.attrib['code'])
+            return True
+    elif 'code' in root_node.attrib:
+       print("[ERROR] Response returned error code: " + root_node.attrib['code'] + "\n" + str(root_node.text))
+       return False
+    elif 'code' not in root_node.attrib:
+        print("[ERROR] Response returned error")
+        return False
+
 #Creates a pkcs12 (.pfx) object/file and uploads to the Palo Alto firewall. Uses credentials.py for API key generation, firewall selection
-def upload_certificate_to_paloalto(private_key: bytes, certificate: bytes, name: str, require_secure_cert: bool):
+def upload_certificate_to_paloalto(private_key: bytes, certificate: bytes, name: str, validate_ssl_certificate: bool):
     #Generates an auth key for subsequent requests
     if not hasattr(credentials, "username"):
         raise Exception("No username specified in credentials.py")
@@ -76,8 +101,13 @@ def upload_certificate_to_paloalto(private_key: bytes, certificate: bytes, name:
 
     #Uses PANOS XMLAPI to import PFX data.
     import_url = "https://" + credentials.hostname + "/api/?type=import&category=keypair&certificate-name=" + cert_friendly_name + "&format=pkcs12&passphrase=" + password
-    response = requests.post(import_url + "&key=" + xmlapi_key, files={'file' : pfx_bytes},verify=require_secure_cert)
+    response = requests.post(import_url + "&key=" + xmlapi_key, files={'file' : pfx_bytes},verify=disable_ssl_validation)
     print(response.text)
+
+def commit_to_panos(hostname: string, api_key: string, validate_ssl_certificate: bool):
+    commit_url = "https://" + hostname + "/api/?type=commit&cmd=<commit></commit>&key=" + api_key
+    response = requests.get(commit_url, verify=validate_ssl_certificate)
+    return check_panos_errors(response.text)
 
 def main():
     json_contents = get_acme_json()
@@ -96,3 +126,86 @@ def main():
                                    common_name, credentials.require_secure_cert)
 
 main()
+
+def get_config_option(args, key_name, default=None):
+    arg = getattr(args, key_name)
+    if arg is not None:
+        return arg
+    elif hasattr(credentials, key_name):
+        return getattr(credentials, key_name)
+    else:
+        return default
+
+def main2():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    parser.add_argument("--username", help="Define username to use to generate API key if not in credentials.py. Ignored if --apikey specified")
+    parser.add_argument("--password", help="Define password to use to generate API key if not in credentials.py. Ignored if --apikey specified")
+    parser.add_argument("--apikey", help="API Key to use when uploading certificate to PANOS device")
+    parser.add_argument("--hostname", help="Hostname of PANOS device to use if not specified in credentials.py")
+    parser.add_argument("--acme_json", help="Location of acme.json certificate storage file if not specified in credentials.py")
+    parser.add_argument("--cert_common_name", help="Common name (CN) of certificate to extract from acme.json if not specified in credentials.py. Uses first found match")
+    parser.add_argument("--cert_output_location", help="Location to store certificate and key if not specified in credentials.py. Ignored if --keep_files not specified")
+    parser.add_argument("--keep_files", help="Delete .crt and.key files after upload. Defaults to False", default=False, action="store_true")
+    parser.add_argument("--disable_ssl_validation", default=False, action="store_true", help="Disables SSL Certificate validation for connecting to PANOS device if set to True. Defaults to False")
+    parser.add_argument("--commit", action="store_true", default=False, help="Commit the firewall/Panorama configuration if set. Note: Does not support Panorama pushing of configuration")
+
+    args = parser.parse_known_args()
+
+    acme_json, hostname, cert_common_name, cert_output_location, api_key = ""
+    disable_ssl_validation = False
+
+    if args.acme_json is not None:
+        acme_json = args.acme_json
+    elif hasattr(credentials, "acme_json"):
+        acme_json = credentials.acme_json
+    else:
+        raise Exception("No ACME JSON location specified. Use --acme_json or specify acme_json=location/to/traefik_acme.json in credentials.py")
+
+    cert_common_name = get_config_option(args, "cert_common_name")
+    if cert_common_name is None:
+        raise Exception("No certificate common name specified. Use --cert_common_name or specify cert_common_name=my_common_name in credentials.py")
+
+    disable_ssl_validation = get_config_option(args, "disable_ssl_validation")
+    if disable_ssl_validation is None:
+        disable_ssl_validation = False
+
+    keep_files = get_config_option(args, "keep_files")
+    if keep_files is None:
+        keep_files = False
+
+    hostname = get_config_option(args, "hostname")
+    if hostname is None:
+        raise Exception("No hostname specified. Use --hostname or specify hostname=mypanoshostname in credentials.py")
+
+    api_key = get_config_option(args, "apikey")
+    if api_key is None and hasattr(credentials, "username") and hasattr(credentials, "password"):
+        api_key = generate_api_key(credentials.username, credentials.password)
+    else:
+        raise Exception("No API key or username+password specified. Use --apikey XXX OR --username myusername --password=mypassword OR \n" + \
+            "Specify apikey=XXX or username=myusername password=mypassword in credentials.py")
+
+    cert_output_location = get_config_option(args, "cert_output_location")
+    commit = get_config_option(args, "commit")
+
+    json_contents = get_acme_json(acme_json)
+    certs = get_certificates(json_contents)
+
+    cert_dct = select_certificate(certs, cert_common_name)
+
+    if cert_dct is None:
+        print("[ERROR] No certificate matching common name " + cert_common_name + " was found in acme.json. Aborting")
+        sys.exit(1)
+    
+    if cert_output_location is not None and keep_files:
+        open(cert_output_location + cert_common_name + ".crt", "wb").write(cert_dct['certificate'])
+        open(cert_output_location + +cert_common_name + ".key", "wb").write(cert_dct['private_key'])
+    
+    upload_certificate_to_paloalto(cert_dct['private_key'], cert_dct['certificate'],
+                                   cert_common_name, not disable_ssl_validation)
+    if commit:
+        if commit_to_panos(hostname, api_key, not disable_ssl_validation):
+            print("[INFO] Commit request sent succesfully")
+    
+
+
